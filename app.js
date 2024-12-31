a85cf2dd43eeb6d3ce8960dd078fa00e3de7317a
Eddie <eulerissmart@gmail.com>
2024-12-31 08:21:13 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const port = 3000;
 
 
 var app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 app.post('/execute', (req, res) => {
   const code = req.body.code;
   const testID = req.body.testID;
